ceecaee58492c63e6d2fd398f7761af9e2a34e38
Richard Hewitson <65354830+richard-scryber@users.noreply.github.com>
2021-10-21 13:20:44 +0100

    Moved the LineSize struct into the OpenType folder and added code comments.

---
+++ Scryber.Core.OpenType/OpenType/LineSize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Scryber.OpenType
+{
+    /// <summary>
+    /// Defines the size of the characters measured and the space needed to fit them from a string measurer
+    /// </summary>
+    public struct LineSize
+    {
+        /// <summary>
+        /// The width reqired to render the fitted characters 
+        /// </summary>
+        public double RequiredWidth { get; private set; }
+
+        /// <summary>
+        /// The height required to render the fitted characters
+        /// </summary>
+        public double RequiredHeight { get; private set; }
+
+        /// <summary>
+        /// The number of characters that were able to fit within the available width.
+        /// </summary>
+        public int CharsFitted { get; private set; }
+
+        /// <summary>
+        /// If true then the characters have been split on a word boundary.
+        /// </summary>
+        public bool OnWordBoudary { get; set; }
+
+
+        /// <summary>
+        /// Creates a new line size value
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="charsfitted"></param>
+        /// <param name="isWordbreak"></param>
+        public LineSize(double width, double height, int charsfitted, bool isWordbreak)
+        {
+            this.RequiredWidth = width;
+            this.RequiredHeight = height;
+            this.CharsFitted = charsfitted;
+            this.OnWordBoudary = isWordbreak;
+        }
+    }
+}
--- Scryber.Core.OpenType/OpenType/TypeMeasureOptions.cs
+++ Scryber.Core.OpenType/OpenType/TypeMeasureOptions.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 namespace Scryber.OpenType
 {
+    /// <summary>
+    /// Defines the options for measuring string sizes
+    /// </summary>
     public class TypeMeasureOptions
     {
+        /// <summary>
+        /// The optional extra spacing between the characters in the proportional size
+        /// </summary>
         public double? CharacterSpacing { get; set; }
 
+        /// <summary>
+        /// The optional extra spacing between the words in the proportional size
+        /// </summary>
         public double? WordSpacing { get; set; }
 
+        /// <summary>
+        /// If true then the closest word will be returned as the required size, rather than all the maximum characters.
+        /// </summary>
         public bool BreakOnWordBoundaries { get; set; }
 
         public TypeMeasureOptions()
